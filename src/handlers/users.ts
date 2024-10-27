4bfc983449410c1aa898e8c1afbb9c7d1c562c3b
shakil-ahmmed-se <shakilahmmed8906@gmail.com>
2024-10-27 15:21:03 +0600

    add some feature

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -9,3 +9,7 @@ export async function getUser(req: Request, res: Response, next: NextFunction){
     const id = parseInt(req.params.id);
     res.send('Hello user')
 }
+
+export async function createUser(req: Request, res: Response, next: NextFunction){
+    res.send('create user');
+}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { getUser, getUsers } from "../handlers/users";
+import { createUser, getUser, getUsers } from "../handlers/users";
 
 
 const router = Router();
-
+// get all users
 router.get('/users', getUsers);
 
+// get a single user
 router.get('/users/:id', getUser);
+
+// create a new user
+router.post('/users', createUser)
 
 export default router;
